6c12636241739cfa3569d4094eba74dac15f10a3
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-10-20 19:06:26 -0300

    Adiciona estrutura do projeto + profile de test com H2

---
+++ desafioapi/src/main/java/com/github/wesleyav/desafioapi/DesafioapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.desafioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioapiApplication.class, args);
+	}
+
+}
